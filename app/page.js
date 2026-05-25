c9d4731c9542f815acb122f6ccf3719366e51d86
tedeyo2016-source <tedeyo2016@gmail.com>
2026-05-25 01:12:33 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -23,6 +23,17 @@ useEffect(() => {
 
   return () => clearInterval(timer);
 }, [])};
+ 
+const [currentImage, setCurrentImage] = useState(0);
+  const images = [
+  "/hero.png",
+  "/open-view.png",
+  "/FurGone Pet on Couch.png",
+  "/FurGone Thank-you card Print_ready.png",
+  "/Easy to you.png"
+];
+
+const [currentImage, setCurrentImage] = useState(0);
 export default function FurGoneWebsite() {
   const [email, setEmail] = useState("");
 const [message, setMessage] = useState("");
